e1d0fae0bf008034fb6f61529b701464a27380c8
Ruchi Jain <ruchismailbox@gmail.com>
2022-01-26 13:56:17 -0600

    Environment setup

---
+++ src/main/java/com/cpbackend/cpbackendapp/CpBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.cpbackend.cpbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpBackendAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpBackendAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cpbackend/cpbackendapp/CpBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpbackend.cpbackendapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpBackendAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
